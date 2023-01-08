7fed6b3157fa1e097a81eb5a6b3d62a575318852
GeneralWafer <miocevichmichael@gmail.com>
2023-01-08 11:47:10 +0000

    updated for pytest

--- test_vulntest.py
+++ test_vulntest.py
@@ -1,4 +1,5 @@
 import requests
+import pytest
 
 def sendPayload(url, signature):
     payload = "dG9rZW49OWdPcXpwQlRwYU9TRkhPdDh3cFNYbUlXJnRlYW1faWQ9VDA0RUY2R0pRSjImdGVhbV9kb21haW49ZXBpY2NvZGluZ2NvbSZjaGFubmVsX2lkPUQwNEVSTVZKTDVBJmNoYW5uZWxfbmFtZT1kaXJlY3RtZXNzYWdlJnVzZXJfaWQ9VTA0RU1RMlJWUUEmdXNlcl9uYW1lPW1pb2NldmljaG1pY2hhZWwmY29tbWFuZD0lMkZ0ZXN0JnRleHQ9aGVsbG8rYWtmbm9rYXdmJTVDJmFwaV9hcHBfaWQ9QTA0RVA0VVQyVVQmaXNfZW50ZXJwcmlzZV9pbnN0YWxsPWZhbHNlJnJlc3BvbnNlX3VybD1odHRwcyUzQSUyRiUyRmhvb2tzLnNsYWNrLmNvbSUyRmNvbW1hbmRzJTJGVDA0RUY2R0pRSjIlMkY0NDk5MjAwNTE0ODM1JTJGeVVMNjdxbmM2Um1OVUVsNFNkZlB0ZldIJnRyaWdnZXJfaWQ9NDQ5MjcxMzM5ODkzNC40NDkxMjIwNjM2NjE0LjJmMTkwZThlOWIxNWEwYzhmNTAxYTJlNjdjZDVmYjI0"
@@ -9,7 +10,6 @@ def sendPayload(url, signature):
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
     return response.text
 
 urlNoHashLamda = "https://9m4lww10jk.execute-api.ap-southeast-2.amazonaws.com/default/noHashLamda"
@@ -23,23 +23,33 @@ noSignature = ''
 accessGranted = 'open sesame'
 accessDenied = 'sesame closed'
 
-#testing with no hash checking
-print("testing noHashLamda...")
-assert sendPayload(urlNoHashLamda, noSignature) == accessGranted
-assert sendPayload(urlNoHashLamda, bogusSignature) == accessGranted
-assert sendPayload(urlNoHashLamda, oldSignature) == accessGranted
-print("\ntests passed!\n")
+#testing noHashLamda with no signature
+def test_noHashLamdaNoSignature():
+    assert sendPayload(urlNoHashLamda, noSignature) == accessDenied
+
+#testing noHashLamda with bogus signature
+def test_noHashLamdaBogusSignature():
+    assert sendPayload(urlNoHashLamda, bogusSignature) == accessDenied
+
+#testing noHashLamda with old signature
+def test_noHashLamdaOldSignature():
+    assert sendPayload(urlNoHashLamda, oldSignature) == accessDenied
 
 #testing with hash checking, but no timestamp check
-print("testing noRepeatCheck...")
-assert sendPayload(urlNoRepeatCheck, noSignature) == accessDenied
-assert sendPayload(urlNoRepeatCheck, bogusSignature) == accessDenied
-assert sendPayload(urlNoRepeatCheck, oldSignature) == accessGranted
-print("\ntests passed!\n")
+def test_noRepeatCheckNoSignature():
+    assert sendPayload(urlNoRepeatCheck, noSignature) == accessDenied
 
-#testing with hash and timestamp checking
-print("testing hashLamda...")
-assert sendPayload(urlHashLamda, noSignature) == accessDenied
-assert sendPayload(urlHashLamda, bogusSignature) == accessDenied
-assert sendPayload(urlHashLamda, oldSignature) == accessDenied
-print("\ntests passed!\n")
+def test_noRepeatCheckBogusSignature():
+    assert sendPayload(urlNoRepeatCheck, bogusSignature) == accessDenied
+
+def test_noRepeatCheckOldSignature():
+    assert sendPayload(urlNoRepeatCheck, oldSignature) == accessDenied
+
+def test_hashLamdaNoSignature():
+    assert sendPayload(urlHashLamda, noSignature) == accessDenied
+
+def test_hashLamdaBogusSignature():
+    assert sendPayload(urlHashLamda, bogusSignature) == accessDenied
+
+def test_hashLamdaOldSignature():
+    assert sendPayload(urlHashLamda, oldSignature) == accessDenied
